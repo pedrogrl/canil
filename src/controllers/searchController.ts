67cc8362ceda6a5c6de4a2241bb57c1515592a52
pedrogrl <pedro.gos@outlook.com>
2022-12-08 13:03:33 -0300

    search improvements

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -5,6 +5,10 @@ import { Pet } from "../models/pet";
 export const search = (req: Request, res: Response) => {
    let query: string = req.query.q as string // form url
 
+   if (!query){
+      res.redirect('back')
+      return
+   }
    res.render('pages/page', {
       menu: '',
       list: Pet.getFromName(query),
