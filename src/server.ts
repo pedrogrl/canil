cb00be20b3a7e37edfc0fe4c7ccb03a457cc5b0e
pedrogrl <pedro.gos@outlook.com>
2022-12-08 09:52:24 -0300

    base

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 
+import mainRoutes from './routes/index'
+
 dotenv.config();
 
 const server = express();
@@ -11,5 +13,10 @@ server.set("views", path.join(__dirname, "views"));
 
 server.use(express.static("public"));
 
+server.use('/', mainRoutes)
+
+server.use((req, res) => {
+   res.send('Page not found!')
+})
 
 server.listen(process.env.PORT);
